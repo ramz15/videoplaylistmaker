d8b862f7d517b065c8816d6d52b90a3cd41b8adc
Andrew Tan <andrew.tan784@gmail.com>
2014-09-25 09:49:16 +0800

    added light styling to index

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -42,12 +42,12 @@ class PlaylistsController < ApplicationController
 
   	def update
   		@playlist = Playlist.find(params[:id])
-    respond_to do |format|
+    	respond_to do |format|
       if @playlist.update_attributes(playlist_params)
-        format.html { redirect_to @playlist, notice: 'PLaylistwas successfully updated.' }
         format.json { render :show, status: :ok, location: @playlist }
       else
         format.html { render :edit }
+        format.html { redirect_to playlists_url, notice: 'Playlist was successfully updated.' }
         format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
