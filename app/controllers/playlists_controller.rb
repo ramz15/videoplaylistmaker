ab12d4e6c4b41d7fc19e7d6e32e494c20e724dcf
Andrew Tan <andrew.tan784@gmail.com>
2014-09-25 08:30:32 +0800

    adding edit and destroys

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -40,6 +40,26 @@ class PlaylistsController < ApplicationController
   		@playlist = Playlist.new
   	end	
 
+  	def update
+    respond_to do |format|
+      if @playlist.update(playlist_params)
+        format.html { redirect_to @playlist, notice: 'PLaylistwas successfully updated.' }
+        format.json { render :show, status: :ok, location: @playlist }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @playlist.destroy
+    respond_to do |format|
+      format.html { redirect_to playlists_url, notice: 'Playlist was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   	private
   	def playlist_params
   		params.require(:playlist).permit(:title, :link)
